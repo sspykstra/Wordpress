752b205864f0ff053fb2c4801c006d957ceefd80
Sebastiaan Spykstra <s.spykstra@gate51.nl>
2015-11-22 17:39:48 +0100

    Remove

--- defaultTheme/footer.php
+++
@@ -1,5 +0,0 @@
-
-	<?php wp_footer(); ?>
-	<a href="#" target="_blank"><i class="fa fa-google-plus"></i></a>
-	<a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
-	<a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
